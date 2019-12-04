50bc619679b2da5f0b286a5ca3ab4fbf34170ad1
feyaq13 <feyaq13@gmail.com>
2019-12-04 23:19:50 +0300

    добавляет поведение при ресайзе окна

--- src/main.js
+++ src/main.js
@@ -19,27 +19,30 @@ $( document ).ready(function() {
   const imgs = document.querySelector(".slider-reviews__wrapper-imgs");
   const btnPrev = document.querySelector(".slider-reviews__prev");
   const btnNext = document.querySelector(".slider-reviews__next");
-
-  const sliderWidth = document.querySelector(".slider-reviews").clientWidth;
-  const slidesWidth = document.querySelectorAll(".slide")[0].clientWidth;
   const counterSlides = 5;
-
-  let slidesLength
-    = (sliderWidth === slidesWidth) ? slidesLength = 5
-    : Math.floor(counterSlides - (sliderWidth / slidesWidth));
-
   let counter = 0;
+  let slidesLength = calcWidth();
 
-  console.log(slidesLength, Math.floor(counterSlides - (sliderWidth / slidesWidth)), sliderWidth, slidesWidth);
+  console.log(slidesLength);
 
   btnNext.addEventListener("click", slideNext);
   btnPrev.addEventListener("click", slidePrev);
-  document.addEventListener("load", checkCounter());
+  document.addEventListener("load", checkButtons());
+  window.addEventListener("resize", calcWidth);
 
-  function checkCounter () {
-    // btnPrev.style.display = (counter === 0) ? "none" : "block";
+  function calcWidth() {
+    let sliderWidth = document.querySelector(".slider-reviews").clientWidth;
+    let slidesWidth = document.querySelectorAll(".slide")[0].clientWidth;
+
+    slidesLength
+      = (sliderWidth === slidesWidth) ? slidesLength = 5
+      : Math.floor(counterSlides - (sliderWidth / slidesWidth));
+
+    return slidesLength
+  }
+
+  function checkButtons() {
     btnPrev.disabled = (counter === 0) ? true : false;
-    // btnNext.style.display = (slidesLength > 0) ? "block" : "none";
     btnNext.disabled = (slidesLength > 0) ? false : true;
   }
 
@@ -48,9 +51,7 @@ $( document ).ready(function() {
       imgs.style.transform += "translate(-100%)";
       ++counter;
 
-      // btnPrev.style.display = (counter === 0) ? "none" : "block";
       btnPrev.disabled = (counter === 0) ? true : false;
-      // btnNext.style.display = (counter < slidesLength - 1) ? "block" : "none";
       btnNext.disabled  = (counter < slidesLength - 1) ? false : true;
     }
   }
@@ -60,11 +61,8 @@ $( document ).ready(function() {
       --counter;
       imgs.style.transform += "translate(100%)";
 
-      // btnPrev.style.display = (counter === 0) ? "none" : "block";
       btnPrev.disabled = (counter === 0) ? true : false;
-      // btnNext.style.display = (counter < slidesLength) ? "block" : "none";
       btnNext.disabled = (counter < slidesLength) ? false : true;
     }
   }
-
 });
