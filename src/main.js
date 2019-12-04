7985dba0f4a62d4cd3499f1b69f02638e4c34c31
feyaq13 <feyaq13@gmail.com>
2019-12-04 18:27:44 +0300

    добавляет слайдер на страницу с заглушками

--- src/main.js
+++ src/main.js
@@ -15,4 +15,49 @@ function showAndPlay() {
   $('.section-video__video').show().get(0).play();
 }
 
+const imgs = document.querySelector(".slider-reviews__wrapper-imgs");
+const btnPrev = document.querySelector(".slider-reviews__prev");
+const btnNext = document.querySelector(".slider-reviews__next");
+
+const sliderWidth = document.querySelector(".slider-reviews").clientWidth;
+const slidesWidth = document.querySelectorAll(".slide")[0].clientWidth;
+const counterSlides = 5;
+
+let slidesLength
+  = (sliderWidth === slidesWidth)
+  ? slidesLength = 5
+  : Math.floor(counterSlides - (sliderWidth / slidesWidth));
+
+let counter = 0;
+
+// console.log(slidesLength);
+
+btnNext.addEventListener("click", slideNext);
+btnPrev.addEventListener("click", slidePrev);
+document.addEventListener("load", checkCounter());
+
+function checkCounter () {
+  btnPrev.style.display = (counter === 0) ? "none" : "block";
+  btnNext.style.display = (slidesLength > 0) ? "block" : "none";
+}
+
+function slideNext() {
+  if (counter < slidesLength) {
+    imgs.style.transform += "translate(-100%)";
+    ++counter;
+
+    btnPrev.style.display = (counter === 0) ? "none" : "block";
+    btnNext.style.display = (counter < slidesLength - 1) ? "block" : "none";
+  }
+}
+
+function slidePrev() {
+  if (counter !== 0) {
+    --counter;
+    imgs.style.transform += "translate(100%)";
+
+    btnPrev.style.display = (counter === 0) ? "none" : "block";
+    btnNext.style.display = (counter < slidesLength) ? "block" : "none";
+  }
+}
 
