836fa9fcccae6715702dfdc49a0e069595e79962
feyaq13 <feyaq13@gmail.com>
2019-12-04 13:35:15 +0300

    добавляет секцию с видео

--- src/main.js
+++ src/main.js
@@ -5,4 +5,14 @@
 //   }
 // });
 
+$('.section-video__button-play').click(showAndPlay);
+
+function showAndPlay() {
+  $('.section-video').css(
+    'background', 'rgba(0, 0, 0, .9)'
+  );
+  $(this).hide();
+  $('.section-video__video').show().get(0).play();
+}
+
 
