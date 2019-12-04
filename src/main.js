a8f7932453a8db638174c0cbb9d8273e1695898a
feyaq13 <feyaq13@gmail.com>
2019-12-04 21:09:44 +0300

    дорабатывает слайдер в отзывах

--- src/main.js
+++ src/main.js
@@ -15,49 +15,50 @@ function showAndPlay() {
   $('.section-video__video').css('visibility', 'visible').get(0).play();
 }
 
-const imgs = document.querySelector(".slider-reviews__wrapper-imgs");
-const btnPrev = document.querySelector(".slider-reviews__prev");
-const btnNext = document.querySelector(".slider-reviews__next");
+$( document ).ready(function() {
+  const imgs = document.querySelector(".slider-reviews__wrapper-imgs");
+  const btnPrev = document.querySelector(".slider-reviews__prev");
+  const btnNext = document.querySelector(".slider-reviews__next");
 
-const sliderWidth = document.querySelector(".slider-reviews").clientWidth;
-const slidesWidth = document.querySelectorAll(".slide")[0].clientWidth;
-const counterSlides = 5;
+  const sliderWidth = document.querySelector(".slider-reviews").clientWidth;
+  const slidesWidth = document.querySelectorAll(".slide")[0].clientWidth;
+  const counterSlides = 5;
 
-let slidesLength
-  = (sliderWidth === slidesWidth)
-  ? slidesLength = 5
-  : Math.floor(counterSlides - (sliderWidth / slidesWidth));
+  let slidesLength
+    = (sliderWidth === slidesWidth) ? slidesLength = 5
+    : Math.floor(counterSlides - (sliderWidth / slidesWidth));
 
-let counter = 0;
+  let counter = 0;
 
-// console.log(slidesLength);
+  console.log(slidesLength, Math.floor(counterSlides - (sliderWidth / slidesWidth)), sliderWidth, slidesWidth);
 
-btnNext.addEventListener("click", slideNext);
-btnPrev.addEventListener("click", slidePrev);
-document.addEventListener("load", checkCounter());
+  btnNext.addEventListener("click", slideNext);
+  btnPrev.addEventListener("click", slidePrev);
+  document.addEventListener("load", checkCounter());
 
-function checkCounter () {
-  btnPrev.style.display = (counter === 0) ? "none" : "block";
-  btnNext.style.display = (slidesLength > 0) ? "block" : "none";
-}
-
-function slideNext() {
-  if (counter < slidesLength) {
-    imgs.style.transform += "translate(-100%)";
-    ++counter;
-
+  function checkCounter () {
     btnPrev.style.display = (counter === 0) ? "none" : "block";
-    btnNext.style.display = (counter < slidesLength - 1) ? "block" : "none";
+    btnNext.style.display = (slidesLength > 0) ? "block" : "none";
   }
-}
 
-function slidePrev() {
-  if (counter !== 0) {
-    --counter;
-    imgs.style.transform += "translate(100%)";
+  function slideNext() {
+    if (counter < slidesLength) {
+      imgs.style.transform += "translate(-100%)";
+      ++counter;
 
-    btnPrev.style.display = (counter === 0) ? "none" : "block";
-    btnNext.style.display = (counter < slidesLength) ? "block" : "none";
+      btnPrev.style.display = (counter === 0) ? "none" : "block";
+      btnNext.style.display = (counter < slidesLength - 1) ? "block" : "none";
+    }
   }
-}
 
+  function slidePrev() {
+    if (counter !== 0) {
+      --counter;
+      imgs.style.transform += "translate(100%)";
+
+      btnPrev.style.display = (counter === 0) ? "none" : "block";
+      btnNext.style.display = (counter < slidesLength) ? "block" : "none";
+    }
+  }
+
+});
