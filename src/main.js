2b1eb750e5da1289c581375b39c35700d9129590
feyaq13 <feyaq13@gmail.com>
2019-12-18 12:45:32 +0300

    правит формулу расчета элементов карусели

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ const $ = require('jquery');
 
 var Swiper = require('swiper').default;
 var mySwiper;
-console.log(mySwiper)
 
 $('.section-video__button-play').click(showAndPlay);
 
@@ -56,52 +55,56 @@ $( document ).ready(function() {
     }
   }
 
-  const imgs = document.querySelector(".slider-reviews__wrapper-imgs");
+  const viewportSlider = document.querySelector(".slider-reviews__wrapper-imgs");
   const btnPrev = document.querySelector(".slider-reviews__prev");
   const btnNext = document.querySelector(".slider-reviews__next");
   const counterSlides = 5;
-  let counter = 0;
-  let slidesLength = calcWidth();
+  let counter = 1;
+  let sliderPagesLength = calcWidth();
 
   btnNext.addEventListener("click", slideNext);
   btnPrev.addEventListener("click", slidePrev);
-  document.addEventListener("load", checkButtons());
+  checkButtons();
   window.addEventListener("resize", calcWidth);
   window.addEventListener("resize", checkButtons);
 
   function calcWidth() {
+    viewportSlider.style.transform = '';
+    counter = 1;
     let sliderWidth = document.querySelector(".slider-reviews").clientWidth;
-    let slidesWidth = document.querySelectorAll(".slide")[0].clientWidth;
+    let slideWidth = document.querySelectorAll(".slide")[0].clientWidth;
+    console.log(sliderWidth, slideWidth);
 
-    slidesLength
-      = (sliderWidth === slidesWidth) ? slidesLength = 5
-      : Math.floor(counterSlides - (sliderWidth / slidesWidth));
+    sliderPagesLength =
+      Math.ceil(counterSlides / (sliderWidth / slideWidth));
 
-    return slidesLength
+    return sliderPagesLength
   }
 
   function checkButtons() {
-    btnPrev.disabled = (counter === 0) ? true : false;
-    btnNext.disabled = (slidesLength > 0) ? false : true;
+    btnPrev.disabled = (counter === 1);
+    btnNext.disabled = (counter === sliderPagesLength);
   }
 
   function slideNext() {
-    if (counter < slidesLength) {
-      imgs.style.transform += "translate(-100%)";
+    if (counter !== sliderPagesLength) {
+      viewportSlider.style.transform += "translate(-100%)";
       ++counter;
+      console.log(counter + '/' + sliderPagesLength);
 
-      btnPrev.disabled = (counter === 0) ? true : false;
-      btnNext.disabled  = (counter < slidesLength - 1) ? false : true;
+      btnPrev.disabled = (counter === 1);
+      btnNext.disabled  = (counter === sliderPagesLength);
     }
   }
 
   function slidePrev() {
-    if (counter !== 0) {
+    if (counter !== 1) {
       --counter;
-      imgs.style.transform += "translate(100%)";
+      console.log(counter + '/' + sliderPagesLength);
+      viewportSlider.style.transform += "translate(100%)";
 
-      btnPrev.disabled = (counter === 0) ? true : false;
-      btnNext.disabled = (counter < slidesLength) ? false : true;
+      btnPrev.disabled = (counter === 1);
+      btnNext.disabled = (counter === sliderPagesLength);
     }
   }
 
